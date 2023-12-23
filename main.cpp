1a91f63634bf1a04ec2f0d1ff938d3a0a0e65812
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2023-12-23 21:16:27 +0300

    Update main.cpp
    Randevu alma sistemi ekledim gün ve saat şeklinde
    dosyaya yazma dosyadan alma fonksiyonları vs.

--- main.cpp
+++ main.cpp
@@ -5,78 +5,78 @@
 #include "Staff.h"
 #include "Doctor.h"
 #include "Nurse.h"
+#include "Hospital.h" // Yeni eklenen Hospital sınıfının başlık dosyası
 
 int main() {
-    Patient Rooms[6][3];
-    string name;
+    Hospital hospital; // Yeni bir Hospital nesnesi oluşturma
+    std::string name;
 
-    int choice=2;
+    int choice = 2;
 
-    do{
-    std::cout << "Welcome to X Hospital!" << std::endl;
-    std::cout << "1 - Randevu alma" << std::endl;
-    std::cout << "2 - Randevu kontrol etme" << std::endl;
-    std::cout << "3 - Doktor giris" << std::endl;
-    std::cout << "4 - Hemsire giris" << std::endl;
-    std::cout << "5 - Staff giris" << std::endl;
-    std::cout << "Your choice: ";
-    std::cin >> choice;
-    switch (choice) {
-        case 0:
-            std::cout << "Cikis ";
-            break;
-        case 1:
-            //
-            int ID,day,doctornumber;
-            int dept,randevu,room;
+    do {
+        std::cout << "Welcome to X Hospital!" << std::endl;
+        std::cout << "1 - Randevu alma" << std::endl;
+        std::cout << "2 - Randevu kontrol etme" << std::endl;
+        std::cout << "3 - Doktor giris" << std::endl;
+        std::cout << "4 - Hemsire giris" << std::endl;
+        std::cout << "5 - Staff giris" << std::endl;
+        std::cout << "0 - Exit" << std::endl;
+        std::cout << "Your choice: ";
+        std::cin >> choice;
 
-            std::cout << "Please enter your ID: ";
-            std::cin >> ID;
-            std::cout << "Please enter your name: ";
-            std::cin >> name;
-            std::cout << "Please enter your desired department" << std::endl;
-            std::cout << "1 - Neurology\n2-KBB\n3 - Cardiology\n Your choice: 1";
-            std::cin >> dept;
-            std::cout << "Select a doctor" << std::endl;
-            std::cout << "1 - Doctor1\n2- Doctor2\n3 - Doctor3\n Your choice: ";
-            //doctor list
-            //std::cin >> doctornumber;
+        switch (choice) {
+            case 0:
+                std::cout << "Exiting ";
+                break;
+            case 1: {
+                int ID, dept, day, hour;
+                std::cout << "Please enter your ID: ";
+                std::cin >> ID;
+                std::cout << "Please enter your name: ";
+                std::cin >> name;
+                std::cout << "Please enter your desired department" << std::endl;
+                std::cout << "1 - Neurology\n2 - KBB\n3 - Cardiology\nYour choice: ";
+                std::cin >> dept;
+                hospital.displayDoctors(dept); // Doktorları görüntüleme
+                std::cout << "Select a doctor (Enter doctor number): ";
+                int doctorNumber;
+                std::cin >> doctorNumber;
 
-            //doctorun boş olduğu saatler
-            //PAZARTESI     SALI        CARSAMBA        PERSEMBE        CUMA        CUMARTESI       PAZAR
-            // Doctor1         .
-            // Doctor2         .           .               .
-            // Doctor3
+                hospital.displayAvailableSlots(doctorNumber); //Doktorların Boş saatleri görüntüleme
+                std::cout << "Enter preferred day: ";
+                std::cin >> day;
+                std::cout << "\nEnter preferred hour: ";
+                std::cin >> hour;
 
-            std::cin >> day >> room;
-            //hastayi vektore at
-            Rooms[day][room] = *new Patient(ID,name);
-            break;
-        case 2:
-            //
-            std::cout << "Please enter your ID" << std::endl;
-            // check rooms vector
-            break;
-        case 3:
-            //Doctor
-            std::cout << "Please enter your ID and password" << std::endl;
-            // Welcome Dr."name"
-            std::cout;
+                hospital.bookAppointment(ID, name, dept, doctorNumber, day, hour); // Randevu alma
+                break;
+            }
+            case 2:
+                //
+                std::cout << "Please enter your ID" << std::endl;
+                // check rooms vector
+                break;
+            case 3:
+                //Doctor
+                std::cout << "Please enter your ID and password" << std::endl;
+                // Welcome Dr."name"
+                std::cout;
 
-            break;
-        case 4:
-            //Nurse
-            std::cout << "Please enter your ID and password" << std::endl;
+                break;
+            case 4:
+                //Nurse
+                std::cout << "Please enter your ID and password" << std::endl;
 
-            break;
-        case 5:
-            //Staff
-            std::cout << "Please enter your ID and password" << std::endl;
+                break;
+            case 5:
+                //Staff
+                std::cout << "Please enter your ID and password" << std::endl;
 
-            break;
+                break;
+            default:
+                std::cout << "Error: Wrong input" << std::endl;
+        }
+    } while (choice != 0);
 
-        default:
-            std::cout << "Error wrong input" << std::endl;
-    }}while(choice!=0);
     return 0;
 }
