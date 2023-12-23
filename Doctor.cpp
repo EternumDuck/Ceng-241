99d71462ce714d5589ad15a4662d57afe4b15966
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2023-12-23 21:18:57 +0300

    Update Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -1,5 +1,29 @@
-//
-// Created by omerh on 12/17/2023.
-//
-
 #include "Doctor.h"
+
+Doctor::Doctor(int number, const std::string& name, int department)
+        : number(number), name(name), department(department) {
+    // İhtiyaç duyulursa başka başlatma işlemleri yapılabilir
+}
+
+int Doctor::getNumber() const {
+    return number;
+}
+
+const std::string& Doctor::getName() const {
+    return name;
+}
+
+int Doctor::getDepartment() const {
+    return department;
+}
+void Doctor::setNumber(int number) {
+    this->number = number;
+}
+
+void Doctor::setName(const std::string& name) {
+    this->name = name;
+}
+
+void Doctor::setDepartment(int department) {
+    this->department = department;
+}
