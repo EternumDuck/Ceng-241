17d412f76741cdb4560bd2754a7531ec29637ac5
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2023-12-23 21:19:56 +0300

    Update Doctor.h

--- Doctor.h
+++ Doctor.h
@@ -1,14 +1,25 @@
-//
-// Created by omerh on 12/17/2023.
-//
-
-#ifndef CENG241_DOCTOR_H
-#define CENG241_DOCTOR_H
-
+#ifndef DOCTOR_H
+#define DOCTOR_H
+#include <vector>
+#include <string>
 
 class Doctor {
+public:
+    Doctor(int number, const std::string& name, int department);
 
+    int getNumber() const;
+    const std::string& getName() const;
+    int getDepartment() const;
+    void setNumber(int number);
+    void setDepartment(int department);
+    void setName(const std::string& name);
+    bool randevu[14][9];
+
+private:
+    int number;
+    std::string name;
+    int department;
+    // Diğer özellikler eklenebilir
 };
 
-
-#endif //CENG241_DOCTOR_H
+#endif // DOCTOR_H
