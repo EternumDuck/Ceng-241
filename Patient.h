c7a716461851959b623413e2d9416d1defb82380
Ömerhan Yiğit <omerhanyigitbaba@gmail.com>
2023-12-24 14:22:16 +0300

    Revert "Update Patient.h"
    This reverts commit 38b5ddd187cde02009f0838303de0f4ca4cadc22.

--- Patient.h
+++ Patient.h
@@ -5,7 +5,6 @@
 using namespace std;
 
 class Patient {
-    private:
     string name;
     string bloodtype;
     int ID,age;
@@ -14,18 +13,12 @@ class Patient {
     vector <string> prescription;
     vector <string> treatment;
 public:
+    Patient(int ID_val,string name_val)
+    :ID{ID_val},name{name_val}{
+    }
     Patient(){
 
     }
-    Patient(int ID, const std::string& name);
-    int getID() const;
-    const std::string& getName() const;
-    void setAge(int age);
-    void setWeight(float weight);
-    void setHeight(float height);
-    void setBloodType(const std::string& bloodtype);
-    void setBloodSugar(float bloodsugar);
-    void setTreatmentCost(float treatmentcost);
 
 };
 
