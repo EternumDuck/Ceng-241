4da9d0ed736b2841fdf9d195e8ab06e7cbcac6bd
Ömerhan Yiğit <omerhanyigitbaba@gmail.com>
2023-12-24 14:22:21 +0300

    Revert "Update Doctor.h"
    This reverts commit 17d412f76741cdb4560bd2754a7531ec29637ac5.

--- Doctor.h
+++ Doctor.h
@@ -1,25 +1,14 @@
-#ifndef DOCTOR_H
-#define DOCTOR_H
-#include <vector>
-#include <string>
+//
+// Created by omerh on 12/17/2023.
+//
+
+#ifndef CENG241_DOCTOR_H
+#define CENG241_DOCTOR_H
+
 
 class Doctor {
-public:
-    Doctor(int number, const std::string& name, int department);
 
-    int getNumber() const;
-    const std::string& getName() const;
-    int getDepartment() const;
-    void setNumber(int number);
-    void setDepartment(int department);
-    void setName(const std::string& name);
-    bool randevu[14][9];
-
-private:
-    int number;
-    std::string name;
-    int department;
-    // Diğer özellikler eklenebilir
 };
 
-#endif // DOCTOR_H
+
+#endif //CENG241_DOCTOR_H
